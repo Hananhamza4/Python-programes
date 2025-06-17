f110e5b44046ec1728e30c51911ff256427ffd7a
Hxnxn <98098743+Hananhamza4@users.noreply.github.com>
2025-06-17 12:04:31 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,26 @@
+# def my_function(lname):
+#     print("david "+lname)
+# my_function("hanan")
+# my_function("toibas")
+# my_function("linux")
+#####deafult function
+# def my_function(a=3,b=4):
+#     sum=a+b
+#     print("sum",sum)
+# my_function(2,3)
+# my_function()
+########key value
+# def display_info(first_name,last_name):
+#     print(first_name,last_name)
+
+# display_info("hanan","mangalath")
+
+# def find_sum(*numbers):
+#     result=0
+#     for i in numbers:
+#         result=result+i
+#     print("sum= ", result)
+
+# find_sum(1,2,3)
+# find_sum(4,9)
+
---
+++ inhertance.py
@@ -0,0 +1,31 @@
+# class Name:
+#     def Na(self,name=None):
+#         if name:
+#             print('hello',name)
+#         else:
+#             print("hello")
+# r1=Name()
+# r1.Na("hanan")
+# r1.Na()            
+
+#above code is method overloading which directly does't support to python
+
+# class Animal:
+#     def sound(self):
+#         print("Animal sound")
+# class Dog(Animal):
+#     def sound(self):
+#         print("boww")
+# class Cat(Animal):
+#     def sound(self):
+#         print("meow")
+
+# a=Animal()
+# a.sound()
+
+# b=Dog()
+# b.sound()
+
+# c=Cat()
+# c.sound()
+
